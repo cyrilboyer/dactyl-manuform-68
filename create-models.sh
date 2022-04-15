b686c3ff0f6d351c133ef142371c398887d6c4be
Cyril Boyer <cyril@skinnyrobot.co.nz>
2022-04-15 15:55:17 +0900

    Adding extra keys at the bottom of row 2 & 4 like the Kinesis Advantage. Adding a bottom plate for 3D printing in resin

--- create-models.sh
+++ create-models.sh
@@ -1,43 +1,43 @@
 lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-4x5.scad
-cp things/left.scad things/left-4x5.scad
-cp things/right-plate.scad things/right-4x5-plate.scad
-openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-4x5.stl things/right-4x5.scad >/dev/null 2>&1 &
-openscad -o things/left-4x5.stl  things/left-4x5.scad >/dev/null 2>&1 &
+cp things/right.scad things/right-cyril.scad
+cp things/left.scad things/left-cyril.scad
+cp things/right-plate.scad things/right-cyril-plate.scad
+/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o things/right-cyril-plate.dxf things/right-cyril-plate.scad
+/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o things/right-cyril.stl things/right-cyril.scad
+/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o things/left-cyril.stl  things/left-cyril.scad
 
-patch -p1 < 4x6.patch 
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-4x6.scad
-cp things/left.scad things/left-4x6.scad
-cp things/right-plate.scad things/right-4x6-plate.scad
-openscad -o things/right-4x6-plate.dxf things/right-4x6-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-4x6.stl things/right-4x6.scad >/dev/null 2>&1  &
-openscad -o things/left-4x6.stl  things/left-4x6.scad >/dev/null 2>&1 &
-git checkout src/dactyl_keyboard/dactyl.clj
+# patch -p1 < 4x6.patch 
+# lein run src/dactyl_keyboard/dactyl.clj
+# cp things/right.scad things/right-4x6.scad
+# cp things/left.scad things/left-4x6.scad
+# cp things/right-plate.scad things/right-4x6-plate.scad
+# openscad -o things/right-4x6-plate.dxf things/right-4x6-plate.scad >/dev/null 2>&1 &
+# openscad -o things/right-4x6.stl things/right-4x6.scad >/dev/null 2>&1  &
+# openscad -o things/left-4x6.stl  things/left-4x6.scad >/dev/null 2>&1 &
+# git checkout src/dactyl_keyboard/dactyl.clj
 
-patch -p1 < 5x6.patch 
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-5x6.scad
-cp things/left.scad things/left-5x6.scad
-cp things/right-plate.scad things/right-5x6-plate.scad
-openscad -o things/right-5x6-plate.dxf things/right-5x6-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-5x6.stl things/right-5x6.scad >/dev/null 2>&1  &
-openscad -o things/left-5x6.stl  things/left-5x6.scad >/dev/null 2>&1 &
-git checkout src/dactyl_keyboard/dactyl.clj
+# patch -p1 < 5x6.patch 
+# lein run src/dactyl_keyboard/dactyl.clj
+# cp things/right.scad things/right-5x6.scad
+# cp things/left.scad things/left-5x6.scad
+# cp things/right-plate.scad things/right-5x6-plate.scad
+# openscad -o things/right-5x6-plate.dxf things/right-5x6-plate.scad >/dev/null 2>&1 &
+# openscad -o things/right-5x6.stl things/right-5x6.scad >/dev/null 2>&1  &
+# openscad -o things/left-5x6.stl  things/left-5x6.scad >/dev/null 2>&1 &
+# git checkout src/dactyl_keyboard/dactyl.clj
 
-patch -p1 < 6x6.patch 
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-6x6.scad
-cp things/left.scad things/left-6x6.scad
-cp things/right-plate.scad things/right-6x6-plate.scad
-openscad -o things/right-6x6-plate.dxf things/right-6x6-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-6x6.stl things/right-6x6.scad >/dev/null 2>&1  &
-openscad -o things/left-6x6.stl  things/left-6x6.scad >/dev/null 2>&1 &
-git checkout src/dactyl_keyboard/dactyl.clj
+# patch -p1 < 6x6.patch 
+# lein run src/dactyl_keyboard/dactyl.clj
+# cp things/right.scad things/right-6x6.scad
+# cp things/left.scad things/left-6x6.scad
+# cp things/right-plate.scad things/right-6x6-plate.scad
+# openscad -o things/right-6x6-plate.dxf things/right-6x6-plate.scad >/dev/null 2>&1 &
+# openscad -o things/right-6x6.stl things/right-6x6.scad >/dev/null 2>&1  &
+# openscad -o things/left-6x6.stl  things/left-6x6.scad >/dev/null 2>&1 &
+# git checkout src/dactyl_keyboard/dactyl.clj
 
 
 # git add things/*-4x5.stl
 # git add things/right-4x5-plate.dxf
 # git commit -m "Add CAD files"
-wait
+# wait
